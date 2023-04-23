2648f252198f1d33013c559df3e338617e431027
XavierSantori <118914055+XavierSantori@users.noreply.github.com>
2023-04-23 21:04:45 +0300

    + Два кода которые нашёл
    Надо будет посмотреть что из них вообще то, а что не то и как оно должно работать

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import cv2
-vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+
+# открываем видеофайл для чтения
+capture = cv2.VideoCapture("video.mp4")
+
+# устанавливаем позицию чтения на 2 секунды (2000 миллисекунд)
+capture.set(cv2.CAP_PROP_POS_MSEC, 2000)
+
+# получаем следующий кадр из видеопотока
+ret, frame = capture.read()
+
+############################################
+
+import cv2
+
+def extract_frame(video_path, frame_number):
+    # Открытие видеофайла
+    video_capture = cv2.VideoCapture(video_path)
+    
+    # Установка позиции кадра
+    video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    
+    # Извлечение кадра
+    ret, frame = video_capture.read()
+    
+    if ret:
+        return frame
+    else:
+        return None
+
+# Пример использования функции
+# Извлечение 10-го кадра из видеофайла "video.mp4"
+frame = extract_frame("video.mp4", 10)
+
+# Отображение кадра
+cv2.imshow("Frame", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
